18e82cb1aca34e553e60804b7442899559aee615
AndrewKoliaka <andrew.koliaka@gmail.com>
2017-03-01 13:23:58 +0200

    gulpfile formatted

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,8 @@
 const gulp = require('gulp');
 const less = require('gulp-less');
-const browserSync = require('browser-sync').create();
 const concat = require('gulp-concat');
 const autoprefixer = require('gulp-autoprefixer');
+const connect = require('gulp-connect');
 
 const pathTo = {
     less: "src/assets/less/**/*.less",
@@ -10,27 +10,35 @@ const pathTo = {
     css: 'src/assets/css/*.css'
 }
 
+gulp.task('connect', () => {
+    connect.server({
+        root: './',
+        port: 3000,
+        livereload: true
+    });
+});
+
 gulp.task('less', () => {
-    gulp.src(pathTo.less)
+    gulp.src(["src/assets/less/components/reset.less", pathTo.less])
         .pipe(concat('styles.css'))
         .pipe(less())
         .pipe(autoprefixer())
-        .pipe(gulp.dest('src/assets/css/'));
+        .pipe(gulp.dest('src/assets/css'))
+        .pipe(connect.reload());
+});
+
+gulp.task('js', () => {
+    connect.reload();
+});
+
+gulp.task('html', () => {
+    connect.reload();
 });
 
 gulp.task('watch', () => {
     gulp.watch(pathTo.less, ['less']);
-    gulp.watch(pathTo.js).on('change', browserSync.reload);
-    gulp.watch(pathTo.css).on('change', browserSync.reload);
-    gulp.watch('index.html').on('change', browserSync.reload);
+    gulp.watch(pathTo.js, ['js']);
+    gulp.watch('index.html', ['html']);
 });
 
-gulp.task('serve', () => {
-    browserSync.init({
-        server: {
-            baseDir: "./"
-        }
-    });
-});
-
-gulp.task('default', ['less', 'serve', 'watch']);
+gulp.task('default', ['less', 'connect', 'watch']);
